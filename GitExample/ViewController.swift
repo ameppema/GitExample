bb40160cd8d493920f102c0df7adac66b2dd037a
Paul Marquez Arica <paul_marquez@icloud.com>
2019-04-22 02:14:52 +0200

    Remove comments & add print

--- GitExample/ViewController.swift
+++ GitExample/ViewController.swift
@@ -1,20 +1,10 @@
-//
-//  ViewController.swift
-//  GitExample
-//
-//  Created by Paul Marquez Arica on 22/04/2019.
-//  Copyright © 2019 pma. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("Este es un proyecto de ejemplo de repp de Git")
     }
-
-
 }
 
